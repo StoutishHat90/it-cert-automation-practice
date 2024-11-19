6cfea28b4c98f539693b1772766e7b8ac75ecfe3
StoutishHat90 <stoutishhat90@gmail.com>
2024-11-19 16:48:34 -0700

    added mailtrap guide version of email script

--- Course6/email_test.py
+++ Course6/email_test.py
@@ -5,22 +5,26 @@ import mimetypes
 import smtplib
 
 #defines the mail server using the local host
+#mail_server = smtplib.SMTP_SSL('google.com')
 mail_server = smtplib.SMTP('localhost')
 
-message = EmailMessage()
-print(message)
 
-sender = "stoutishhat90@gmail.com"
-recipient = "bdawespetrie@gmail.com"
 
-message['From'] = sender
-message['To'] = recipient
-print(message)
+# message = EmailMessage()
+# print(message)
 
-message['Subject'] = 'Greetings from {} to {}!'.format(sender, recipient)
-print(message)
+# sender = "stoutishhat90@gmail.com"
+# recipient = "bdawespetrie@gmail.com"
+# password = "password"
 
-body = """Hey there!
+# message['From'] = sender
+# message['To'] = recipient
+# print(message)
 
-I'm learning to send emails using Python!"""
-message.set_content(body)
+# message['Subject'] = 'Greetings from {} to {}!'.format(sender, recipient)
+# print(message)
+
+# body = """Hey there!
+
+# I'm learning to send emails using Python!"""
+# message.set_content(body)
